a2c3cf5bd45eafde2d5a57b2c86a9c49ce64cbf1
rhquee <kfrak@proarray.com>
2019-01-03 13:14:02 +0100

    just working

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import repository.User;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
