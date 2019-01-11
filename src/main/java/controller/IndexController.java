605fea21adbf78f6e07b39fb1f52b790fb9fa889
rhquee <kfrak@proarray.com>
2019-01-11 14:54:47 +0100

    /

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -1,11 +1,9 @@
 package controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-import repository.User;
 import repository.UserDTO;
 
 /**
@@ -14,17 +12,13 @@ import repository.UserDTO;
 @Controller
 public class IndexController {
 
-    @Autowired
-    User user;
-
     @GetMapping(value = {"/", "/index"})
     public ModelAndView index(Model model) {
         ModelAndView modelAndView = new ModelAndView();
         if (!model.containsAttribute("loginForm")) {
             model.addAttribute("loginForm", new UserDTO());
         }
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("password", user.getPassword());
+        model.addAttribute("username");
 //        modelAndView.addObject("username");
         modelAndView.setViewName("index");
         return modelAndView;
--- src/main/java/service/AuthenticationValidator.java
+++ src/main/java/service/AuthenticationValidator.java
@@ -2,13 +2,16 @@ package service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import repository.UserDetailsServiceImplementation;
 
 /**
  * Created by kfrak on 11.01.2019.
  */
+@Service
 public class AuthenticationValidator {
     @Autowired
-    UserDetails userDetails;
+    UserDetailsServiceImplementation userDetailsServiceImplementation;
 
     public boolean checkIfUserSuccessLogedIn(UserDetails userDetails, String password) {
         if (userDetails.getPassword().equals(password)) {
